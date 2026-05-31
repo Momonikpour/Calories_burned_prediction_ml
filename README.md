c3c9f4791debc1f1b4453c6b14fcb5cd34a92743
Momonikpour <mohammadnikpour123@gmail.com>
2026-06-01 01:09:49 +0300

    Feat: Clean files ready for cloud deployment

---
+++ README.md
@@ -0,0 +1,61 @@
+## 🔥 Calories Burnt Prediction | End-to-End Machine Learning Project
+
+Built an end-to-end machine learning application using **Python** to predict calories burnt during a workout based on biological and workout-related features.
+
+App Link : https://calories-burnt-machine-learning.onrender.com
+---
+
+## 🚀 Project Highlights
+
+- Designed a complete machine learning pipeline:  
+  **Data Ingestion → Data Transformation → Model Training → Prediction**
+- Designed and implemented data preprocessing pipelines using **One-Hot Encoding** and **Standard Scaler**
+- Trained and compared multiple models:
+  - Linear Regression  
+  - Decision Tree Regressor  
+  - Random Forest Regressor  
+- Performed **hyperparameter tuning using GridSearchCV** to select the best-performing model
+- Built a **Flask web application** for real-time calorie predictions
+- Learned **Docker fundamentals** and containerized the application
+- Deployed the Dockerized application on **Microsoft Azure Web App** shown in another git repository.
+- Deployed the live web application on **Render** for public access
+
+---
+
+## 🛠️ Tech Stack
+
+- **Language:** Python  
+- **Libraries:** NumPy, Scikit-learn  
+- **Model:** Random Forest Regressor  
+- **Framework:** Flask  
+- **Deployment:** Render  
+
+---
+
+## 📊 Prediction Features
+
+- Age  
+- Height  
+- Weight  
+- Workout Duration  
+- Heart Rate  
+- Body Temperature
+
+# Code
+<img width="1891" height="1006" alt="Screenshot 2026-01-29 202727" src="https://github.com/user-attachments/assets/adeb23bb-29bc-4507-a626-ab14ef5791ee" />
+
+# App Interface
+<img width="1600" height="853" alt="Screenshot 2026-01-29 202800" src="https://github.com/user-attachments/assets/d7252aab-a6cc-4ca2-9a09-c107493c9c72" />
+
+# Prediction
+<img width="1666" height="840" alt="Screenshot 2026-01-29 202810" src="https://github.com/user-attachments/assets/13ec3fb8-a055-4d1c-9c09-3970168a0cbc" />
+
+## Render Deployment
+
+https://github.com/user-attachments/assets/8217d44c-ad1c-4db1-a249-e84e193865df
+
+## Microsoft Azure Deployment
+
+https://github.com/user-attachments/assets/339f45b9-0091-479b-86ce-25c271ee17e2
+
+
---
+++ src/utils.py
@@ -0,0 +1,71 @@
+# Utils contains common functionality which all the entire project can use
+import os
+import sys
+from sklearn.base import clone
+
+import numpy as np 
+import pandas as pd
+import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+from src.exception import CustomException
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+        # write in binary mode
+        # The with statement ensures the file is automatically closed after writing.
+        # open(file_path, "wb") opens or creates the file exactly at the location given in file_path.
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
+        trained_models = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model_name = list(models.keys())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv=3, n_jobs=1,refit = False)
+            # Refit = False it does not train a final model it only computes best_params 
+            gs.fit(X_train,y_train)
+
+            # Hyperparameter search only
+            best_model = clone(model)
+            best_model.set_params(**gs.best_params_)
+            best_model.fit(X_train, y_train)
+
+            # ** → Python’s “dictionary unpacking” operator. It expands the dictionary into keyword arguments.
+            # ** = “Take all key-value pairs from a dictionary and pass them as named parameters.”
+            # If gs.best_params_ is: {"max_depth": 10, "n_estimators": 200}
+            # eg python does model.set_params(max_depth=10, n_estimators=200)
+            
+            y_test_pred = best_model.predict(X_test)
+            
+            test_score = r2_score(y_test, y_test_pred)
+            
+            trained_models[model_name] = best_model
+            # test score are model prediction
+            report[list(models.keys())[i]] = test_score
+
+        return report, trained_models
+    
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)
